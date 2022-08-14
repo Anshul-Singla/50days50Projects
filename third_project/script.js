d7225debbd965821416099e2a26e9c0df2d9b76b
Anshul-Singla <asingla2807@gmail.com>
2022-08-15 01:24:45 +0530

    thirdProject

---
+++ third_project/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+const close = document.getElementById('close');
+const open = document.getElementById('open').addEventListener('click',()=>container.classList.add('show-nav'));
+
+close.addEventListener('click',()=>container.classList.remove('show-nav'))
+// console.log(open);
+// console.log(close)
+
