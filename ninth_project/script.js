777492b3ee39eed9d3641e4a31dc22d163efbd53
Anshul-Singla <asingla2807@gmail.com>
2022-11-10 23:58:37 +0530

    style & script added

--- ninth_project/script.js
+++ ninth_project/script.js
@@ -4,5 +4,18 @@ soundsButtons.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound ;
+
+    btn.addEventListener("click" , () => {
+        stopSongs();
+        document.getElementById(sound).play()
+    })
+
     document.getElementById('buttons').append(btn)
-})
+});
+const stopSongs = () => {
+    soundsButtons.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0
+    })
+}
