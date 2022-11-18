cbaf3be14d69708ac6f88783791428874e08d7e5
Anshul-Singla <asingla2807@gmail.com>
2022-11-19 01:01:22 +0530

    JS done

---
+++ tenth_project/script.js
@@ -0,0 +1,31 @@
+let jokeEl = document.getElementById('joke');
+let jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', ()=>{
+    getJoke()
+})
+
+// .then syntax
+const getJoke = () => {
+    fetch('https://icanhazdadjoke.com/' , {
+        headers:{
+            Accept : "application/json",
+        },
+    }).then(res => res.json())
+    .then(data => 
+        jokeEl.innerHTML = data.joke
+        )
+};
+
+// async await syntax
+const getJok = async() => {
+   const res = await fetch('https://icanhazdadjoke.com/' , {
+        headers:{
+            Accept : "application/json",
+        },
+    })
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke
+    // console.log('res:', res)
+
+};
+getJok();
